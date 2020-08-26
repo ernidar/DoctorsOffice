a0039198b4d5496071534618c4ba86c40702021c
ernidar <ernidar20gmail.com>
2020-08-26 11:54:31 -0400

    Doctor's office program

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         String patientName = "Stacey Manson";
+        String appDate = "5/10/22";
+        String contactNumber ="5715250520";
         boolean hasInsurance = false;
         double temp = 102.2;
+        double copay = 20.00;
         int numVisits = 5;
 
         System.out.println("Patient Details:");
@@ -12,5 +15,7 @@ public class Main {
         System.out.println("Patient has insurance: " + hasInsurance);
         System.out.println("Body temprature: " + temp);
         System.out.println("Number of visits : " + numVisits);
+        System.out.println("Copay amount : $" + copay + "0");
+        System.out.println("");
     }
 }
